d533d18e1372947f8eb8bc17e95cf2a2e7469efa
Ben <voben21@gmail.com>
2025-12-15 17:07:11 -0800

    added logging

--- target/debug/deps/raytracing_rust-ad16e7e635c085be.d
+++
@@ -1,5 +0,0 @@
-/home/hyde/Programming/rust/raytracing-rust/target/debug/deps/raytracing_rust-ad16e7e635c085be.d: src/main.rs
-
-/home/hyde/Programming/rust/raytracing-rust/target/debug/deps/libraytracing_rust-ad16e7e635c085be.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/raytracing-rust.d
+++
@@ -1,1 +0,0 @@
-/home/hyde/Programming/rust/raytracing-rust/target/debug/raytracing-rust: /home/hyde/Programming/rust/raytracing-rust/src/main.rs
